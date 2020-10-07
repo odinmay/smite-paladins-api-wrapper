355071992363685294d621f34c452e54e2c0de41
Odin May <odinmerlinmay@gmail.com>
2020-10-07 18:19:37 -0400

    Add methods for api calls, and update docstrings

--- smite_api_wrapper.py
+++ smite_api_wrapper.py
@@ -31,17 +31,17 @@ class SmiteAPI:
         return hashlib.md5(self.dev_id.encode('utf-8') + methodname.encode('utf-8') +
                            self.auth_key.encode('utf-8') + timestamp.encode('utf-8')).hexdigest()
 
-    def create_session(self):
+    def _create_session(self):
         """Creates and Updates the session, and self.session.id"""
         sig = self._create_signature('createsession')
         url = f'{self.base_url}/createsessionJson/{self.dev_id}/{sig}/{self._create_timestamp()}'
         req = requests.get(url).json()
         self.session_id = req['session_id']
 
-    def test_session(self) -> True:
+    def _test_session(self) -> True:
         """Checks if there is a current valid session, if not it creates one"""
         if self.session_id is None:
-            self.create_session()
+            self._create_session()
             return True
         else:
             url = f"{base_url}testsession{response_frmt}/{self.dev_id}/" \
@@ -50,21 +50,21 @@ class SmiteAPI:
             if 'successful' in req.text:
                 return True
             else:
-                self.create_session()
+                self._create_session()
                 print('Restarting session')
                 return True
 
-    def create_request(self, methodname: str, params=None):
+    def _create_request(self, methodname: str, params=None):
         """Takes the method and parameters which are then built into a url and requested, returns json"""
-        if self.test_session():
-            url = self.create_url(methodname, params)
+        if self._test_session():
+            url = self._create_url(methodname, params)
             print(url)
             req = requests.get(url)
             return json.loads(req.text, encoding='utf-8')
         else:
-            self.create_session()
+            self._create_session()
 
-    def create_url(self, methodname, params=()) -> str:
+    def _create_url(self, methodname, params=()) -> str:
         """Builds and returns the url to be requested when given the parameters and methodname"""
         signature = self._create_signature(methodname)
         timestamp = self._create_timestamp()
@@ -77,32 +77,71 @@ class SmiteAPI:
                     partial_url.append(str(param))
         return base_url + '/'.join(partial_url)
 
+    def get_patch_info(self) -> dict:
+        """Function returns information about current deployed patch.
+         Currently, this information only includes patch version"""
+        data = self._create_request('getpatchinfo')
+        return data
+
     def get_gods(self) -> list:
-        """Return Json of every God and their stats"""
-        data = self.create_request('getgods', [self.lang_code])
+        """Returns all Gods and their various attributes"""
+        data = self._create_request('getgods', [self.lang_code])
         return data
 
-    def get_player(self, player_name: List[str]) -> list:
-        """Return Json of a requested player by name"""
-        data = self.create_request('getplayer', player_name)
-        return data
-
-    def get_god_skins(self, god_id: List[str]):
-        """Return Json of all skins for particular god by ID"""
-        data = self.create_request('getgodskins', [god_id, [self.lang_code]])
+    def get_god_skins(self, god_id: List[str]) -> list:
+        """Returns all available skins for a particular God"""
+        data = self._create_request('getgodskins', [god_id, self.lang_code])
         return data
 
     def get_god_leaderboard(self, god_id: List[str], queue: List[str]) -> list:
-        """Return Json leaderboard data for a specific god and queue"""
-        data = self.create_request('getgodleaderboard', [god_id, queue])
+        """Returns the current season’s leaderboard for a god/queue combination"""
+        data = self._create_request('getgodleaderboard', [god_id, queue])
         return data
 
     def get_god_recommended_items(self, god_id: List[str]) -> list:
-        """Return recommended items for a particular god by ID"""
-        data = self.create_request('getgodrecommendeditems', [god_id, self.lang_code])
+        """Returns the Recommended Items for a particular God"""
+        data = self._create_request('getgodrecommendeditems', [god_id, self.lang_code])
         return data
 
     def get_items(self) -> list:
-        """Return all items in Smite"""
-        data = self.create_request('getitems', [self.lang_code])
+        """Returns all Items and their various attributes"""
+        data = self._create_request('getitems', [self.lang_code])
+        return data
+
+    def get_player(self, player_name: List[str]) -> list:
+        """Returns league and other high level data for a particular player"""
+        data = self._create_request('getplayer', [player_name])
+        return data
+
+    def get_player_id(self, player_name: List[str]) -> list:
+        """Returns a player id, which is used for other function calls"""
+        data = self._create_request('getplayeridbyname', [player_name])
+        try:
+            return [data[0]['player_id']]
+        except KeyError:
+            print('Player not found')
+
+    def get_friends(self, player_id: list) -> list:
+        """Returns the Smite User names of each of the player’s friends. [PC only]"""
+        data = self._create_request('getfriends', [player_id])
+        return data
+
+    def get_god_ranks(self, player_id: list) -> list:
+        """Returns the Rank and Worshippers value for each God a player has played"""
+        data = self._create_request('getgodranks', [player_id])
+        return data
+
+    def get_player_achievements(self, player_id: list) -> dict:
+        """Returns select achievement totals for the specified playerId"""
+        data = self._create_request('getplayerachievements', [player_id])
+        return data
+
+    def get_player_status(self, player_id: list) -> list:
+        """Returns players current online status"""
+        data = self._create_request('getplayerstatus', [player_id])
+        return data
+
+    def get_match_history(self, player_id: list) -> list:
+        """Gets recent matches and high level match statistics for a particular player"""
+        data = self._create_request('getmatchhistory', [player_id])
         return data
